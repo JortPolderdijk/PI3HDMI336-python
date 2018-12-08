62de30034db93d1dde50e1c71cbf5fb4cfc15316
Jort Polderdijk <j.polderdijk@student.fontys.nl>
2018-12-08 14:42:22 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/user/bin/env python
+
+from setuptools import setup, find_packages
+
+import sys
+
+setup(name		 = 'PI3HDMI336',
+      version 		 = '1.0',
+      author   		 = 'Jort Polderdijk',
+      author_email 	 = 'j.polderdijk@student.fontys.nl',
+      description	 = 'This is a library to control the PI3HDMI336 over I2C.',
+      licence		 = '',
+      url 		 = 'https://github.com/JortPolderdijk/PI3HDMI336-python',
+      install_requires	 = ['Adafruit-GPIO'],
+      packages 		 = find_packages());
